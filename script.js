45e797e4ed4379880952d3c8757b99d13f47467d
Geghard-S <geghard@gmail.com>
2021-06-08 23:58:25 -0700

    first commit

--- script.js
+++ script.js
@@ -17,12 +17,13 @@ function setColor(element) {
     const color = getRandomColor()
     element.style.background = color
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
-}
+ }
 
-fuction removeColor(element) {
-
-}
-
-function getRandomColor() {
-    return color[Math.floor(Math.random() * colors.length)]
-}
+ function removeColor(element) {
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+ }
+ 
+ function getRandomColor() {
+     return colors[Math.floor(Math.random() * colors.length)]
+ }
